fbadd6dee8f11dffcc2135270287323f5f921414
basjavabas <32512187+basjavabas@users.noreply.github.com>
2023-05-10 13:08:47 +0300

    first commit

---
+++ lib/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
